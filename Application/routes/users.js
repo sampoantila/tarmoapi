d35e2ed2b24e4f6a4c3c12d65176feb4539bea1c
Sampo Antila <sampo.antila@vincit.fi>
2019-06-27 18:33:38 +0300

    json result for api

--- Application/routes/users.js
+++ Application/routes/users.js
@@ -6,7 +6,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', async (req, res) => {
     let result = await dbservice.list();
-    res.send(result);
+    //res.send(result);
+    res.json(result);
 });
 
 export default router;
